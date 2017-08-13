60b3ba50992b9503fa09c2903203a93f8d1da561
AmitShmuel <aamits7@gmail.com>
2017-08-13 19:10:21 +0300

    enable-migrations

--- Vidlyyyy/App_Start/BundleConfig.cs
+++ Vidlyyyy/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace Vidlyyyy
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-lumen.css",
                       "~/Content/site.css"));
         }
     }
---
+++ Vidlyyyy/Controllers/MoviesController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Vidlyyyy.Models;
+using Vidlyyyy.ViewModels;
+using System.Linq;
+
+namespace Vidlyyyy.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movie = GetMovies().SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
+
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+    }
+}
